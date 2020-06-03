313ec68b5e981f63f7f7d3d99f768c154a40e629
aliensmart <kaoua17@gmail.com>
2020-06-03 14:44:30 -0400

    added ratingTemplate file

---
+++ src/components/ratingTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ratingTemplate = props=>{
+    return(
+        <div className="rating">
+            <span className="star"></span>
+            <span className="star"></span>
+            <span className="star"></span>
+            <span className="star"></span>
+            <span className="star"></span>
+        </div>
+    )
+}
+
+export default ratingTemplate;
